1d5797f067e31c56e9c99683c741ffb2dc2c3f9c
Alejandro Ziade <alejandroziade@gmail.com>
2024-06-07 17:45:26 -0300

    added api

---
+++ Api/firstapi/PrimerApi/Mappings/MappingProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using PrimerApi.Dto;
+using PrimerApi.Models;
+
+namespace PrimerApi.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Usuario, UsuarioDto>();
+    }
+}
---
+++ Api/firstapi/PrimerApi/Models/MarcaAvion.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PrimerApi.Models;
+[Table("marcas_aviones")]
+public class MarcaAvion
+{
+    public Guid Id { get; set; }
+    public string Nombre { get; set; }
+    public DateTime FechaAlta { get; set; }
+}
